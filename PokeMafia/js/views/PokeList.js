32977a21c54f3e9e4ff9d45c3929656644db1953
Kitcat-Alt <streetfion65@gmail.com>
2026-03-25 10:58:09 +0100

    style de la page poke liste

--- PokeMafia/js/views/PokeList.js
+++ PokeMafia/js/views/PokeList.js
@@ -10,22 +10,30 @@ export class PokeList {
       
       const pokemons = request["results"];
       
-      let html = "";
+      let html = `<div class="container mt-4">
+                    <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4">`;
 
       for(const pokemon in pokemons){
         let poke = pokemons[pokemon]
         let poke_note = await this.pokeProvider.fetchPokeNote(this.getPokeId(poke.url))
         let sprite = await this.pokeProvider.fetchPokeSprite(this.getPokeId(poke.url))
         console.log(sprite);
-        html += ` <div class="card"> 
-                      <h2> ${poke.name}</h2>
-                      <img src="${sprite}" alt="${poke.name} front" class="img-fluid">
-                      <p> ${poke_note} </p>
-                  </div>
-        `
-
+        html += ` 
+          <div class="col">
+            <div class="card h-100 shadow-sm text-center">
+              <div class="card-body d-flex flex-column align-items-center justify-content-center">
+                <img src="${sprite}" alt="${poke.name} front" class="img-fluid mb-3" style="max-width: 96px;">
+                <h5 class="card-title text-capitalize mb-3">${poke.name}</h5>
+                <span class="badge bg-primary mt-auto fs-6">Note : ${poke_note}</span>
+              </div>
+            </div>
+          </div>
+        `;
       }
     
+      html += `   </div>
+                </div>`;
+                
       return html;
 
     } catch (error) {
